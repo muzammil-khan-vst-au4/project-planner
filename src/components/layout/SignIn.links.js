86a8e719af7e237f3722edb70c516383a52e3e62
muzammil-khan-vst-au4 <muzammil.khan@outlook.in>
2020-03-23 15:53:54 +0530

    redux setup

--- src/components/layout/SignIn.links.js
+++ src/components/layout/SignIn.links.js
@@ -3,7 +3,7 @@ import {NavLink} from 'react-router-dom'
 export default function SignInLinks() {
     return (
         <ul className="right">
-            <li><NavLink to="/">New Project</NavLink></li>
+            <li><NavLink to="/create">New Project</NavLink></li>
             <li><NavLink to="/">Logout</NavLink></li>
             <li><NavLink to="/" className="btn btn-floating pink lighten-1">NN</NavLink></li>
         </ul>
--- src/index.js
+++ src/index.js
@@ -1,10 +1,18 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import './index.css'
+import {createStore, } from 'redux'
+import {Provider} from 'react-redux'
+import rootReducer from './store/rootReducer'
+
+const store = createStore(rootReducer)
 
 ReactDOM.render(
-  <React.StrictMode>
-    <App />
-  </React.StrictMode>,
+  <Provider store={store}>
+    <React.StrictMode>
+      <App />
+    </React.StrictMode>
+  </Provider>,
   document.getElementById('root')
 );
